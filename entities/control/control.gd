e40824dad1650c620ddd1b803f8cb0e7c8acea13
Emilien Leroy <emilien1005@hotmail.fr>
2023-01-31 07:05:54 +0100

    ✨ Add rotation using an second touch stick
    Also move sticks into a control scene

---
+++ entities/control/control.gd
@@ -0,0 +1,4 @@
+class_name ControlTouch extends Node2D
+
+onready var left = $Left;
+onready var right = $Right;
--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,16 +1,20 @@
 extends KinematicBody2D
 
-export (NodePath) var joystick_left_path;
-onready var left : VirtualJoystick = get_node(joystick_left_path)
+export (NodePath) var control_path;
+onready var control: ControlTouch = get_node(control_path);
 
 var speed = 250;
 var velocity = Vector2();
 
 func _physics_process(delta):
-	if left and left.is_pressed():
-		position += get_position_touch(left.get_output(), speed, delta);
-	else:
+	if control.left and control.left.is_pressed():
+		position += get_position_touch(control.left.get_output(), speed, delta);
+	#
+	if move_keys_is_pressed():
 		move_and_collide(get_direction_keys(velocity, speed, delta));
+	
+	if control.right and control.right.is_pressed():
+		rotation = control.right.get_output().angle();
 
 func get_position_touch(v: Vector2, s: int, d: float):
 	return v * s * d;
@@ -28,3 +32,11 @@ func get_direction_keys(v: Vector2, s: int, d: float):
 		v.y -= 1
 	
 	return  v.normalized() * s * d;
+
+func move_keys_is_pressed():
+	var right = Input.is_action_pressed('ui_right'); 
+	var left = Input.is_action_pressed('ui_left');
+	var down = Input.is_action_pressed('ui_down');
+	var up = Input.is_action_pressed('ui_up');
+	
+	return right or left or down or up;
