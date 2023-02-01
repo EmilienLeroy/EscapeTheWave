0e3702aff035cd303b723b4989e858b2242b71e1
Emilien Leroy <emilien1005@hotmail.fr>
2023-02-01 07:25:51 +0100

    ✨ Add base bullet and add it to the player

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,9 +1,12 @@
 extends KinematicBody2D
 
+var Bullet = preload("res://entities/bullets/base/base.tscn");
+
 export (NodePath) var control_path;
 onready var control: ControlTouch = get_node(control_path);
 
 var speed = 250;
+var shoot_interval = 10;
 var velocity = Vector2();
 
 func _physics_process(delta):
@@ -15,6 +18,10 @@ func _physics_process(delta):
 	
 	if control.right and control.right.is_pressed():
 		rotation = control.right.get_output().angle();
+		
+func _process(delta):
+	if (control.right and control.right.is_pressed() and Engine.get_idle_frames() % shoot_interval == 0):
+		shoot();
 
 func get_position_touch(v: Vector2, s: int, d: float):
 	return v * s * d;
@@ -40,3 +47,8 @@ func move_keys_is_pressed():
 	var up = Input.is_action_pressed('ui_up');
 	
 	return right or left or down or up;
+
+func shoot():
+	var bullet = Bullet.instance();
+	owner.add_child(bullet);
+	bullet.transform = $Muzzle.global_transform;
