42a7c6986cb45da62d2b32683bdb5a76e8a07816
Emilien Leroy <emilien1005@hotmail.fr>
2023-02-03 06:54:38 +0100

    ✨ Destroy bullet on collision
    Also limit the frame rate at 60 to get the same number of shoot per second on each device

--- entities/bullets/base/base.gd
+++ entities/bullets/base/base.gd
@@ -2,5 +2,11 @@ extends Area2D
 
 export var speed = 700;
 
+func _ready():
+	connect("body_entered", self, "on_body_enter");
+
 func _physics_process(delta):
 	position += transform.x * speed * delta
+
+func on_body_enter(body):
+	queue_free();
---
+++ main.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+func _ready():
+	Engine.set_target_fps(60);
+	pass
+
