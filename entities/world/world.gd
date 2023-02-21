c0b680bf09324148ad1675ddc62e622bd4247d5b
Emilien Leroy <emilien1005@hotmail.fr>
2023-02-21 07:39:44 +0100

    🚧 Update wall generation using walker aglo

--- entities/world/world.gd
+++ entities/world/world.gd
@@ -1,22 +1,65 @@
 extends Node2D
 
+const DIRECTIONS = [Vector2.RIGHT, Vector2.UP, Vector2.LEFT, Vector2.DOWN]
 
 export var width = 40;
 export var height = 40;
-
-var noise;
+ 
+var start = Vector2(width / 4, height / 4);
+var direction = Vector2.RIGHT
+var borders = Rect2(0, 0, width, height)
+var walls = [];
+var walls_since_turn = 0;
+var rooms = []
 
 func _ready():
-	var noise = OpenSimplexNoise.new();
-
-	noise.seed = randi();
-	noise.octaves = 1.0;
-	noise.period = 20;
+	walls.append(start);
 	
-	add_wall(noise);
+	add_wall(walk(height * 10));
 	add_grass();
 	
 	pass
+
+func walk(steps):
+	for step in steps:
+		if walls_since_turn >= 8:
+			change_direction()
+		
+		if step():
+			walls.append(start)
+		else:
+			change_direction()
+	return walls
+	
+func step():
+	var target_position = start + direction
+	if borders.has_point(target_position):
+		walls_since_turn += 1
+		start = target_position
+		return true
+	else:
+		return false
+
+func change_direction():
+	walls_since_turn = 0
+	var directions = DIRECTIONS.duplicate()
+	directions.erase(direction)
+	directions.shuffle()
+	direction = directions.pop_front()
+	while not borders.has_point(start + direction):
+		direction = directions.pop_front()
+
+func place_room(position):
+	var size = Vector2(randi() % 4 + 2, randi() % 4 + 2)
+	var top_left_corner = (position - size/2).ceil();
+	
+	rooms.append({position = position, size = size})
+	
+	for y in size.y:
+		for x in size.x:
+			var new_step = top_left_corner + Vector2(x, y)
+			if borders.has_point(new_step):
+				walls.append(new_step)
 
 func add_grass():
 	for x in width:
@@ -26,11 +69,8 @@ func add_grass():
 	$Grass.update_bitmask_region(Vector2(0.0, 0.0), Vector2(width, height));
 
 
-func add_wall(noise):
-	for x in width:
-		for y in height: 
-			var a = noise.get_noise_2d(x, y)
-			if a < 0.3 and a > 0.05:
-				$Wall.set_cell(x, y, 0);
-	
+func add_wall(map):
+	for location in map:
+		$Wall.set_cellv(location, 0);
+
 	$Wall.update_bitmask_region(Vector2(0.0, 0.0), Vector2(width, height));
